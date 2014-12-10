893a11494d3d0113893263d907dea93c35295066
Joao Carreira <joao@berkeley.edu>
2014-12-09 23:37:00 -0800

    Log x axis. Looks better

--- images_graphs/batchsize_vs_latency/plot.r
+++ images_graphs/batchsize_vs_latency/plot.r
@@ -2,6 +2,8 @@ input <- read.table("data", header=TRUE)
 
 pdf("batchsize_vs_latency.pdf")
 
-plot(input$x, input$y, main="End-to-end latency by Batch Window", xlab="Batch window size (ms)", ylab="End-to-end latency (ms)")
+plot(input$x, input$y, main="End-to-end latency by Batch Window", xlab="Batch window size (ms)", ylab="End-to-end latency (ms)", xaxt="n", log="x", yaxt="n")
+axis(1, at = c(6,10,20,30,40,50))
+axis(2, at = c(100,150,200,250,300,350))
 lines(input$x, input$y)
 
