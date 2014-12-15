25babce186ca600187a5d5caeece526c9a3047ee
Joao Carreira <joao@berkeley.edu>
2014-12-15 14:01:01 -0800

    Fixed Sec->Msec in poster and plot script

--- images_graphs/optimizations/graph2/plot1.r
+++ images_graphs/optimizations/graph2/plot1.r
@@ -4,7 +4,7 @@ pdf("task_deser_micro.pdf")
 data <- matrix(c(2.1,1.5,0.035,0.011,0.12,0.013), ncol=2, nrow=3)
 
 barplot(data, main="Effect of optimizations on task deserialization",
-        ylab="Time (s)", xlab="", col=c("darkblue","darkgreen", "darkred"), 
+        ylab="Time (ms)", xlab="", col=c("darkblue","darkgreen", "darkred"), 
         beside=FALSE, space=10,
         names.arg=c("Eager instantiation and broadcast", "Lazy instantiation and cache"), 
         legend=c("Update Dependencies","Deserialize Binary","Other"))
