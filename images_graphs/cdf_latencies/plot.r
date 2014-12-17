de9772d4ebb9a83dec90e39ff88ab4f3ec010b8f
Joao Carreira <joao@berkeley.edu>
2014-12-17 01:27:30 -0800

    Another pass. New graph

---
+++ images_graphs/cdf_latencies/plot.r
@@ -0,0 +1,18 @@
+data <- scan("e2e_times_30mbs_10ms")
+data2 <- scan("e2e_times_90mbs_10ms")
+
+pdf("cdf_e2e_times.pdf")
+
+#options(scipen=5)
+l <- ecdf(data2)
+
+first <- ecdf(data)
+#plot(first)
+plot.ecdf(data, xlim=c(1, 2e2), main="CDF of latencies (10ms batch)", xlab="Latency (ms)", ylab="CDF", col="darkgreen")
+
+#line <- plot.ecdf(data2, log="x", xlim=c(1, 2e2), main="CDF of latencies (batch 6 ms)", xlab="Latency (ms)", ylab="CDF")
+lines(l, type="S", col="darkred")
+#axis(1, at = c(10,100))
+#ylim=c(1, 1e6))
+
+#dev.off()
