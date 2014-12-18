4780910387247fd2ce5b127633300ab15e1ca4b1
Joao Carreira <joao@berkeley.edu>
2014-12-18 01:22:10 -0800

    Reorganized graphs. New reference.

--- images_graphs/cdf_latencies/plot.r
+++ images_graphs/cdf_latencies/plot.r
@@ -8,10 +8,10 @@ l <- ecdf(data2)
 
 first <- ecdf(data)
 #plot(first)
-plot.ecdf(data, xlim=c(1, 2e2), main="CDF of latencies (10ms batch)", xlab="Latency (ms)", ylab="CDF", col="darkgreen")
+plot.ecdf(data, xlim=c(1, 2e2), main="CDF of latencies (10ms batch)", xlab="Latency (ms)", ylab="CDF", col="gray14")
 
 #line <- plot.ecdf(data2, log="x", xlim=c(1, 2e2), main="CDF of latencies (batch 6 ms)", xlab="Latency (ms)", ylab="CDF")
-lines(l, type="S", col="darkred")
+lines(l, type="S", col="gray63")
 #axis(1, at = c(10,100))
 #ylim=c(1, 1e6))
 
